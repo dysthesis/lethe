528ab52c2e01989243f28750d5fcb5d69e1cd05b
Dysthesis <antheoraviel@protonmail.com>
2026-03-17 12:52:05 +0700

    Minor fixes

--- crates/lethe-cli/src/main.rs
+++ crates/lethe-cli/src/main.rs
@@ -1,6 +1,7 @@
 use std::{env, str::FromStr};
 
 use clap::Parser;
+use color_eyre::eyre::eyre;
 use lethe_core::{identifier::Identifier, note::Note};
 
 use crate::cli::Cli;
@@ -9,13 +10,12 @@ mod cli;
 fn main() -> color_eyre::eyre::Result<()> {
     color_eyre::install()?;
     let cli = Cli::parse();
-    // TODO: FIX UNWRAP!!!
-    let root = cli.dir.or_else(|| env::current_dir().ok()).unwrap();
+    let root = cli.dir.or_else(|| env::current_dir().ok()).ok_or_else(|| eyre!("Could not determine current working directory, and none was provided in the command-line arguments!"))?;
     let res = match cli.command {
         cli::Command::New { body, aliases } => Note::new(root, body, aliases.unwrap_or_default()),
         cli::Command::Read { id } => {
             let trimmed = id.trim();
-            Note::read(Identifier::from_str(&trimmed)?, root)
+            Note::read(Identifier::from_str(trimmed)?, root)
         }
     }?;
 
